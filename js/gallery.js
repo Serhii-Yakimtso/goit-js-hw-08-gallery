c7fc2e1780a3430d8a6b1f5eef95e746272cfcb3
Serhii-Yakimtso <serafyso@gmail.com>
2020-10-19 16:41:24 +0300

    Открываем модальное окно
    с отображением оригиналоного размера выбранного изображения

--- js/gallery.js
+++ js/gallery.js
@@ -1,18 +1,38 @@
+// Получаем исходные данные из файла "gallery-items.js"
 import galleryItems from "./gallery-items.js";
 
-console.log("Исходные данные галлереи");
-console.log(galleryItems);
+// console.log("Исходные данные галлереи");
+// console.log(galleryItems);
 
+// Поиск списка галлереи в разметке
 const galleryList = document.querySelector(".js-gallery");
-console.log(galleryList);
+// console.log(galleryList);
 
-console.log("Создаем элементы списка");
+// Создаем элементы списка
 const galleryListItems = createGalleryListItems(galleryItems);
-console.log(galleryListItems);
+// console.log(galleryListItems);
 
-console.log("Влаживаем элементы списка в разметку");
+// Поиск модального окна в разметке
+const modal = document.querySelector(".js-lightbox");
+// console.log(modal);
+
+// Поиск блока отображения оригиналоного изображения в модальном окне
+const imageView = document.querySelector(".lightbox__image");
+
+// Поиск кнопки закрытия модального окна в разметке
+const closeBtn = document.querySelector(".lightbox__button");
+// console.log(closeBtn);
+
+// Влаживаем элементы списка в разметку
 galleryList.insertAdjacentHTML("beforeend", galleryListItems);
 
+// Открываем модальное окно
+galleryList.addEventListener("click", galleryImageClick);
+
+// Закрываем модальное окно
+closeBtn.addEventListener("click", closeModal);
+
+// Функции
 function createGalleryListItems(gallery) {
   return gallery
     .map(({ preview, original, description }) => {
@@ -33,4 +53,45 @@ function createGalleryListItems(gallery) {
 `;
     })
     .join("");
+}
+
+function galleryImageClick(evt) {
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  evt.preventDefault();
+
+  openModal(evt);
+}
+
+function openModal(evt) {
+  modal.classList.add("is-open");
+
+  selectedImageView(evt);
+}
+
+function selectedImageView(evt) {
+  const selectedImage = evt.target;
+  // console.log(selectedImage);
+
+  const hrefSelectedImage = selectedImage.dataset.source;
+  // console.log(hrefSelectedImage);
+
+  const altSelectedImage = selectedImage.alt;
+  // console.log(altSelectedImage);
+
+  imageView.src = hrefSelectedImage;
+  imageView.alt = altSelectedImage;
+}
+
+function closeModal() {
+  modal.classList.remove("is-open");
+
+  clearingAtributesOfSelectedImage();
+}
+
+function clearingAtributesOfSelectedImage() {
+  imageView.src = "";
+  imageView.alt = "";
 }
